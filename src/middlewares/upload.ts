9afd44402161121cb64572afbebe064e53a938a8
sittipong112054 <66011212054@msu.ac.th>
2025-10-28 04:37:52 +0700

    test12

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -2,9 +2,11 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-
 const UPLOAD_ROOT = path.join(process.cwd(), 'uploads');
-const AVATAR_DIR = path.join(UPLOAD_ROOT, 'avatars');
+const AVATAR_DIR_LOCAL = path.join(UPLOAD_ROOT, 'avatars');
+
+const IS_RENDER = !!process.env.RENDER || process.env.NODE_ENV === 'production';
+const AVATAR_DIR = IS_RENDER ? '/tmp/avatars' : AVATAR_DIR_LOCAL;
 
 fs.mkdirSync(AVATAR_DIR, { recursive: true });
 
